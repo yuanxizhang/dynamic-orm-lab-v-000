a57350ff4a3c82beb5e11f3404a11e77ab1ad330
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 04:02:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  self.column_names.each {|c| attr_accessor c.to_sym}
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
