d1dd3567b972124144a5e360c157eb7e0a230ab1
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 04:07:27 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,7 +28,7 @@ class InteractiveRecord
      self.class.table_name
   end
   
-  def column_names_for_insert
+  def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
   
