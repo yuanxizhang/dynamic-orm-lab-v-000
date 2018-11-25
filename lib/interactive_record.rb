4cc5a2d84ecda0dd9156a08d7574140e90e89426
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 04:59:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end
   
   def self.find_by(options)
-    sql = "SELECT * FROM #{self.table_name} WHERE '#{options.keys[0]}' = #{options.vlues[0]""
+    sql = "SELECT * FROM #{self.table_name} WHERE #{options.keys
     DB[:conn].execute(sql)
   end
 end
