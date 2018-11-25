662107becf49ef5cc32c65ea2b8204f8f6107014
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 04:08:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,6 +41,7 @@ class InteractiveRecord
   def save 
     sql = "INSERT INTO #{table_name_for_insert} (#{column_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
+    @id = 
   end
   
   def self.find_by_name(name)
