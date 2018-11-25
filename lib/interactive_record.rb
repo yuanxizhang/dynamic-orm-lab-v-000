1d6992bf21ea56baf26cdb2b0871061768ce1b5c
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 03:33:53 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,7 +23,7 @@ class InteractiveRecord
   end
   
   def column_names_for_insert
-    
+    self.class.
   
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
