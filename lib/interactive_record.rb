f3a30cf6d0d957b8b1d618593f0313ef88e5c8f1
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 03:17:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,12 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+		self.class.to_s.downcase.pluralize
+	end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 end
