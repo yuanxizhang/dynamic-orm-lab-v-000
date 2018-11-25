7521ac3c89702ebdc8076031bfc660d3e5953867
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-25 05:24:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,8 +52,9 @@ class InteractiveRecord
   def self.find_by(options)
     key_list = options.keys
     volue_list = options.values
+    kay_value_list = []
     options.each {|k, v|  key_Value_list << "#{k} = #{v}"}
-    sql = "SELECT * FROM #{self.table_name} WHERE #{key_list[0]} = #{value_list[0]"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key_value_list[0]"
     DB[:conn].execute(sql)
   end
 end
